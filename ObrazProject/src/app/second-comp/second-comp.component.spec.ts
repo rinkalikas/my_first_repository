3b08cfb87b1ca9483487163bae7bbd12ee274ed6
rinkalika <miharukoto@gmail.com>
2020-11-24 15:17:12 +0700

    unit test 1st commit

--- ObrazProject/src/app/second-comp/second-comp.component.spec.ts
+++ ObrazProject/src/app/second-comp/second-comp.component.spec.ts
@@ -1,10 +1,13 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LessonsService } from '../lessons.service';
 
 import { SecondCompComponent } from './second-comp.component';
 
 describe('SecondCompComponent', () => {
   let component: SecondCompComponent;
   let fixture: ComponentFixture<SecondCompComponent>;
+  // let lessonsService = jasmine.createSpyObj('LessonsService', )
+  let service: LessonsService 
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
@@ -17,6 +20,12 @@ describe('SecondCompComponent', () => {
     fixture = TestBed.createComponent(SecondCompComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+     TestBed.configureTestingModule({
+       providers:[{
+         provide: LessonsService, useValue: service
+       }]
+     })
+    service = TestBed.inject(LessonsService)
   });
 
   it('should create', () => {
